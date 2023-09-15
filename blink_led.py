72f30bb9c60308894e135a8bde631833fc697353
Ashish Kumar <ash.c3po@gmail.com>
2023-09-15 16:00:54 -0400

    Adding blink_led.py

---
+++ blink_led.py
@@ -0,0 +1,11 @@
+from gpiozero import LED
+from time import sleep
+
+#Refer to the gpio.jpg for pin layout.
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(0.5)
+    led.off()
+    sleep(0.25)
