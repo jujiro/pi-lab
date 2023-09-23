338c1f2e371b6d05c230402b93769f537c0171f1
Ashish Kumar <ash.c3po@gmail.com>
2023-09-23 16:10:56 -0400

    Greetings from ubuntui9

--- thread.py
+++ thread.py
@@ -1,4 +1,3 @@
-import logging
 import threading
 import time
 import random
@@ -9,30 +8,25 @@ x=[4]
 def thread_function(name):
     print(f"Thread {name}: starting")
     while True:
-      p.append(name)
-      print(f"Thread {name}: in loop {p} id={id(p)}")      
+      print(f"Thread {name}")      
       time.sleep(1)
       #break
     print(f"Thread {name}: finishing")
  
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
- 
-    logging.info("Main    : before creating thread")
     x = threading.Thread(target=thread_function, args=("Hello",))
-    logging.info("Main    : before running thread 1")
     x.start()
     y = threading.Thread(target=thread_function, args=("There",))
-    logging.info("Main    : before running thread 2")
     y.start()
- 
-    logging.info("Main    : wait for the thread to finish")
-    input("Press Enter to stop...")
-    x.terminate()
-    y.terminate()
-    x.join()
-    y.join()
-    logging.info("x=%s", x)
-    logging.info("Main    : all done")
+
+    input("Press Enter to stop...\n")
+    print("Quitting")
+    
+    # Join or no join, there is no convenient way to kill the threads.
+    # Kill and terminate attributes would be nice. They exist for processes only.
+    #x.join()
+    #y.join()
+    
+    
+    
+
