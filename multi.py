cbdc681c82595f47a118cffd049309c7efac9669
Ashish Kumar <ash.c3po@gmail.com>
2023-09-23 15:26:14 -0400

    Modified multi.py

--- multi.py
+++ multi.py
@@ -6,50 +6,34 @@ import random
 import sys
 import pickle
 
+pp1=None
+pp2=None
 
-def print_shared():
-  print(p)
-
-def thread_function(p):
+def thread_function():
   while True:
-    i=random.randrange(0,7)
-    print(i)
-    #p[i]=[True, False, "ABCDEFGHTTTT", b"998sssssss9", pickle.dumps({"a":4, "b":6}), 77.987, None][i] 
-    p[i]=['howdyTT', b'HoWdYTT', -273.154, 100, "DDDD", 99, b"998sssssss9"][i] 
+    print(f"Hello from thread. {pp1}")
     time.sleep(2)
       
 def process_controller():
   sys.stdin = open(0) # <--- Here's the magic line...you'll need to "import sys" above too
+  global pp1, pp2
+  pp2 = Process(target=thread_function, args=())
+  pp2.start()   
   while True:
-    key = input("Enter a string: ")
+    key = input("Enter x to quit")
     if key=="x":
-      print(f"You entered {key}")
+      pp1.kill()
+      pp2.kill()
+      print(f"Hello from controller. {pp1}")
       break
  
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    smm=SharedMemoryManager()
-    smm.start()
-    global p
-    lst=['howdy', b'HoWdY', -273.154, 100, None, "x", b"998"]
-    print(lst)
-    p=smm.ShareableList(lst)
+  pp1 = Process(target=thread_function, args=())
+  pp1.start()
+  #pp2 = Process(target=thread_function, args=())
+  #pp2.start() 
+  z=Process(target=process_controller, args=())
+  z.start()
+  z.join()
+  print ("Exiting")
 
-    print(p)
-    z=Process(target=process_controller, args=())
-    z.start()
-
-    x = Process(target=thread_function, args=(p,))
-    x.start()
-    
-    y = Process(target=thread_function, args=(p,))
-    y.start()
-    z.join()
-            
-    x.terminate()
-    y.terminate()
-    x.join()
-    y.join()
-
-    print(p)
-    print_shared()
