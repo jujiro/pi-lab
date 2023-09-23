88827683767925a3d0cc3df6df454355860e88ae
Ashish Kumar <ash.c3po@gmail.com>
2023-09-23 15:22:47 -0400

    Adding better button

--- better_button.py
+++ better_button.py
@@ -1,12 +1,25 @@
 # Example: Button
 from gpiozero import Button
-from time import sleep
+from time import sleep, perf_counter
 
-#Refer to the gpio.jpg for pin layout.
-btn = Button(17)
+def process_button_pressed(btn):
+  print(f"Pressed {perf_counter()} {btn.is_pressed} {btn.is_held}")
 
-while True:
-	btn.wait_for_press()
-	print("Button pressed")
-	break
+def process_button_held(btn):
+  print(f"Held {perf_counter()}")
+
+def process_button_released(btn):
+  print(f"Released {perf_counter()}")
+
+def main():
+  b=Button(17, hold_time=2)
+  b.hold_repeat=False
+  b.when_pressed = process_button_pressed
+  b.when_held = process_button_held
+  b.when_released = process_button_released
+  while True:
+    sleep(1)
+  
+if __name__ == "__main__":
+  main()
 
