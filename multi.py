cd9d1e31e67a97bf24d3f1ea938a602388e225b4
Ashish Kumar <ash.c3po@gmail.com>
2023-09-23 15:40:55 -0400

    Made multi.py better

--- multi.py
+++ multi.py
@@ -9,31 +9,29 @@ import pickle
 pp1=None
 pp2=None
 
-def thread_function():
+def child(my_name):
   while True:
-    print(f"Hello from thread. {pp1}")
+    print(f"Hello from thread, {my_name}")
     time.sleep(2)
       
-def process_controller():
+def controller():
   sys.stdin = open(0) # <--- Here's the magic line...you'll need to "import sys" above too
   global pp1, pp2
-  pp2 = Process(target=thread_function, args=())
+  pp2 = Process(target=child, args=("Dick",))
   pp2.start()   
   while True:
-    key = input("Enter x to quit")
+    key = input("Enter x to quit\n")
     if key=="x":
       pp1.kill()
       pp2.kill()
-      print(f"Hello from controller. {pp1}")
+      print(f"Uh oh. Time to go.")
       break
- 
+
 if __name__ == "__main__":
-  pp1 = Process(target=thread_function, args=())
+  pp1 = Process(target=child, args=("Tom",))
   pp1.start()
-  #pp2 = Process(target=thread_function, args=())
-  #pp2.start() 
-  z=Process(target=process_controller, args=())
-  z.start()
-  z.join()
+  c=Process(target=controller, args=())
+  c.start()
+  c.join()
   print ("Exiting")
 
